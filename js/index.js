bd0f096f6b0c3027c85f9f8a1e40e05339ca071e
ashan0510 <asHan0510@gmail.com>
2022-05-16 17:31:19 -0400

    Jquery added
    Jquery was added for empathy and design.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,15 @@
-console.log("Your index.js file is loaded correctly!");
+$(".skillEmpathy").hover (function (){
+    console.log("You hovered");
+    $("#empathyText").css("display", "block");
+},
+function(){
+    $("#empathyText").css("display", "none");
+});
+
+$(".skillDesign").hover (function (){
+    console.log("You hovered");
+    $("#designText").css("display", "block");
+},
+function(){
+    $("#designText").css("display", "none");
+});
